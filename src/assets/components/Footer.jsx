9439e869bd065b758af69ce0301b2fc4c8249b3b
l-martinetti <luca.martinetti@outlook.it>
2025-01-19 23:20:39 +0100

    feat: add footer
    -Excercise complete

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,6 +1,200 @@
 const Footer = () => {
     return (
-        <footer>f</footer>
+        <footer>
+            <section id="topFooter">
+                <div className="container">
+                    <div className="topFooterMenus">
+                        <ul className="dcComics">
+                            <h4>dc comics</h4>
+                            <li>
+                                <a href="#">
+                                    <span>characters</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>comics</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>movies</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>TV</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>games</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>videos</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>news</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul>
+                            <h4>shop</h4>
+                            <li>
+                                <a href="#">
+                                    <span>shop DC</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>shop DC collectibles</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="topFooterMenus dc">
+                        <ul>
+                            <h4>dc</h4>
+                            <li>
+                                <a href="#">
+                                    <span>terms of use</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>Privacy policy (New)</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>ad choices</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>advertising</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>jobs</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>subscriptions</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>talent workshops</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>CPSC certificates</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>ratings</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>shop help</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>contact us</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="topFooterMenus sites">
+                        <ul>
+                            <h4>sites</h4>
+                            <li>
+                                <a href="#">
+                                    <span>DC</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>MAD Magazine</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC Kids</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC Universe</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span>DC power visa</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-img-container">
+                        <div>
+                            <img src="/src/assets/img/dc-logo-bg.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="bottomFooter">
+                <div className="container d-flex justify-space-between">
+                    <div className="bottomFooterBtn">
+                        <a href="#">
+                            <span>sign-up now!</span>
+                        </a>
+                    </div>
+                    <div className="fotterMenuContainer">
+                        <ul>
+                            <li>
+                                <span>follow us</span>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/img/footer-facebook.png" alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/img/footer-twitter.png" alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/img/footer-youtube.png" alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/img/footer-pinterest.png" alt="" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="/src/assets/img/footer-periscope.png" alt="" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </footer>
     )
 }
 
